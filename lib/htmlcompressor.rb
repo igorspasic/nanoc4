cab6b1b7efcf68485322023b9dfcbfbec3ba2b9e
Igor Spasić <igor@jodd.org>
2015-09-17 16:33:54 +0200

    added html compressor

---
+++ lib/htmlcompressor.rb
@@ -0,0 +1,15 @@
+require 'nanoc-external'
+
+class HtmlCompressorFilter < Nanoc::Filter
+  identifier :htmlcompressor
+
+  def run(content, params = {})
+    Nanoc::External::Filter.new.run(
+      content,
+      params = {
+        :exec => 'htmlcompressor',
+        :options => %w(-t html)
+      }
+    )
+  end
+end
